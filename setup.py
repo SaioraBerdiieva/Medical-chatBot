8d6a43b393b7ebe6a2ee8ac9e7f3055648e4cdd0
Sai51-49 <pohsara19@gmail.com>
2025-03-08 20:23:48 +0100

    created folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI ChatBot',
+    version= '0.0.0',
+    author= 'Berdiiev Saiora',
+    author_email= 'berdiievas@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
